1f569e5c4631e693417e05c72970de74b1208c75
suzyalfer <105470223+suzyalfer@users.noreply.github.com>
2024-03-28 08:40:25 -0300

    Add files via upload
    Publicação_V1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+var btnMenu = document.querySelector('.btn-menu');
+var menu = document.querySelector('.menu');
+
+
+btnMenu.addEventListener('click', function(){
+
+    menu.classList.toggle('menu-open');
+   
+    
+})
